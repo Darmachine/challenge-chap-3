0113f6a778871743db696aac390bee103d98ca9c
Darmachine <nurdinadarmadi@gmail.com>
2021-10-08 22:21:18 +0700

    Update skripjawakuno.js

--- game suit/skripjawakuno.js
+++ game suit/skripjawakuno.js
@@ -1,67 +1,43 @@
-// DOM
-const selectionButton = document.querySelectorAll("[data-selection]");
-const SELECTION = [{
-        name: "r",
-        beats: "s",
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [{
+        name: 'rock',
+        beats: 'scissors'
     },
     {
-        name: "p",
-        beats: "r",
+        name: 'paper',
+        beats: 'rock'
     },
     {
-        name: "s",
-        beats: "p",
-    },
-];
+        name: 'scissors',
+        beats: 'paper'
+    }
+]
 
-selectionButton.forEach((selectionButton) => {
-    selectionButton.addEventListener("click", (e) => {
-        const selectionName = selectionButton.dataset.selection;
-        const selection = SELECTION.find((selection) => selection.name === selectionName);
-        makeSelection(SELECTION);
-    });
-});
-
-function randomSelection() {
-    const randomIndex = Math.floor(Math.random() * SELECTION.length);
-    return SELECTION[randomIndex];
-}
-
-function isWinner(selection, opponentSelection) {
-    return selection.beats === opponentSelection.name;
-}
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+    })
+})
 
 function makeSelection(selection) {
-    const computerSelection = randomSelection();
-    const yourWinner = isWinner(selection, computerSelection);
-    const computerWinner = isWinner(computerSelection, selection);
-
-    addSelectionResult(computerSelection, computerWinner);
-    addSelectionResult(selection, yourWinner);
-    console.log(selection)
-    console.log(computerSelection)
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
 
     if (yourWinner) {
         document.getElementById("Versus").src = "../assets/images/You Win.png";
-    }
-    if (computerWinner) {
+    } else if (computerWinner) {
         document.getElementById("Versus").src = "../assets/images/You Lose.png";
-    }
-
-    function addSelectionResult(selection, winner) {
-        if (yourWinner) {
-            document.getElementById("Versus").src = "../assets/images/You Win.png";
-        } else {
-            document.getElementById("Versus").src = "../assets/images/You Lose.png";
-        }
-    }
+    } else { console.log = "kuda Terbank" }
 }
 
 function isWinner(selection, opponentSelection) {
-    return selection.beats === opponentSelection.name;
+    return selection.beats === opponentSelection.name
 }
 
 function randomSelection() {
-    const randomIndex = selectionMath.floor(Math.random() * SELECTION.length);
-    return SELECTION[randomIndex];
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
 }
